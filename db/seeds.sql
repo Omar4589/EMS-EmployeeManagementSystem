1695e4e86a33738ccdf548a458388386cdd877be
Omar4589 <omar4589@hotmail.com>
2023-04-18 10:43:08 -0500

    completes index.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,11 +16,11 @@ VALUES ('General Manager', 100000, 1),
 
 -- Insert initial data into the employee table
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Tom', 'Ran', 1, 1),
-       ('Karlos', 'Sol', 2, 2),
-       ('Aldo', 'Ram', 3, 3),
-       ('Luis', 'Mad', 4, 4),
-       ('Javi', 'Dav', 5, NULL),
-       ('Omar', 'Zum', 6, NULL),
-       ('Luis', 'Gar', 7, NULL),
-       ('Dan', 'Ram', 8, NULL);
+VALUES ('Tom', 'Ran', 1, NULL),
+       ('Karlos', 'Sol', 2, 1),
+       ('Aldo', 'Ram', 3, 1),
+       ('Luis', 'Mad', 4, 2),
+       ('Javi', 'Dav', 5, 2),
+       ('Omar', 'Zum', 6, 2),
+       ('Luis', 'Gar', 7, 2),
+       ('Dan', 'Ram', 8, 2);
